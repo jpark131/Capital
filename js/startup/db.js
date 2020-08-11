a5ade4df8e61660219ba0fe4dbb5afa273e94ba4
Jake Park <43481924+jpark131@users.noreply.github.com>
2020-08-10 19:12:47 -0700

    Fixed db connection string

--- js/startup/db.js
+++ js/startup/db.js
@@ -1,9 +1,11 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
+const winston = require("winston");
+const mongoose = require("mongoose");
 
-module.exports = function() {
-    const db = 'dmongodb+srv://capitalUser:1234@capital.q4dxd.azure.mongodb.net/capital?retryWrites=true&w=majority';
-    mongoose.set('useCreateIndex', true);
-    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+module.exports = function () {
+  const db =
+    "dmongodb+srv://capitalUser:12345@capital.q4dxd.azure.mongodb.net/capital?retryWrites=true&w=majority";
+  mongoose.set("useCreateIndex", true);
+  mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => winston.info(`Connected to ${db}...`));
-}
+};
