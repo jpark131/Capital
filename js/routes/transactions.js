9f5b35029104dca7e1cbe77f68312d0c69493ea1
Jake Park <43481924+jpark131@users.noreply.github.com>
2020-08-12 09:16:54 -0700

    Updated put method

--- js/routes/transactions.js
+++ js/routes/transactions.js
@@ -4,12 +4,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", auth, async (req, res) => {
-  const transactions = await Transaction.find().sort("date");
+  const transactions = await Transaction.find().sort("+date");
   res.send(transactions);
 });
 
 router.get("/:id", auth, async (req, res) => {
   const transaction = await Transaction.findById(req.params.id);
+  if (!transaction) res.status(404).send("The transaction could not be found");
   res.send(transaction);
 });
 
@@ -26,6 +27,30 @@ router.post("/", auth, async (req, res) => {
   });
 
   transaction = await transaction.save();
+  res.send(transaction);
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const transaction = await Transaction.findByIdAndUpdate(
+    req.params.id,
+    {
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      business: req.body.business,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!transaction) res.status(404).send("The transaction could not be found");
+
   res.send(transaction);
 });
 
