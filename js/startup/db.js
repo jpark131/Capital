7546168c07f14c60ba2efe43aec094136bcbc7da
Jake Park <43481924+jpark131@users.noreply.github.com>
2020-08-12 11:44:25 -0700

    Removed a logging feature that was causing an error

--- js/startup/db.js
+++ js/startup/db.js
@@ -1,9 +1,9 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function () {
-  const db =
-    "dmongodb+srv://capitalUser:12345@capital.q4dxd.azure.mongodb.net/capital?retryWrites=true&w=majority";
+  const db = config.get("db");
   mongoose.set("useCreateIndex", true);
   mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
