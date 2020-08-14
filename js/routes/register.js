6e0e52eec775fe5f5ca49a34c3feae046bf4a882
Nathan Holland <nwhollan@calpoly.edu>
2020-08-14 09:50:29 -0600

    Home page css and html

--- js/routes/register.js
+++ js/routes/register.js
@@ -5,6 +5,24 @@ const mongoose = require('mongoose');
 
 router.post('/', async (req, res) => {
     //register code here
+    // validates user by calling validate method.
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // checks if user is already in the database by looking up email for
+    // other registered users with the same email
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+
+    user = new User ({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    await user.save();
+
+    res.send(user);
 });
 
 module.exports = router;
